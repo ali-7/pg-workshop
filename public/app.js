04b5df04c16ec9e2f2083713339c5a5040fbf1dc
ali <ali.7@outlook.com>
2019-08-15 03:37:24 +0300

    use-fetch-api

--- public/app.js
+++ public/app.js
@@ -1,35 +1,15 @@
-/* generic XHR request */
-function request(url, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      cb(null, xhr.responseText);
-    } else {
-      cb('error' + xhr.responseType);
-    }
-  };
-  xhr.open('GET', url, true);
-  xhr.send();
-}
-
-function updateDom(err, data) {
-  if (err) {
-    console.error(err);
-  } else {
-    var users = JSON.parse(data);
-    var table = document.getElementById('users-table');
-    /* create a row in table for each user returned from DB */
-    users.forEach(function(user) {
-      var row = document.createElement('tr');
-      var name = document.createElement('td');
-      name.innerHTML = user.name;
+fetch("/users")
+  .then(res => res.json())
+  .then(users => {
+    const table = document.getElementById("users-table");
+    users.forEach(user => {
+      const row = document.createElement("tr");
+      const name = document.createElement("td");
+      name.textContent = user.name;
       row.appendChild(name);
-      var location = document.createElement('td');
-      location.innerHTML = user.location;
+      const location = document.createElement("td");
+      location.textContent = user.location;
       row.appendChild(location);
       table.appendChild(row);
     });
-  }
-}
-
-request('/users', updateDom);
+  });
